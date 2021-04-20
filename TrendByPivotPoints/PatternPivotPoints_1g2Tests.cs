ff6eaa7395e4fd97976111e0c99df91bd9ecc72e
Сабитов Ильяс Ильдарович <sabitov.ilyas@gmail.com>
2021-04-20 14:25:00 +1000

    Реализовал 2 паттерна PivotPoints: 1g2, 1l2

---
+++ TrendByPivotPoints/PatternPivotPoints_1g2Tests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrendByPivotPoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendByPivotPoints.Tests
+{
+    [TestClass()]
+    public class PatternPivotPoints_1g2Tests
+    {
+        [TestMethod()]
+        public void CheckTest_1g2()
+        {
+            //arrange
+            var pattern = new PatternPivotPoints_1g2();
+            var extremums = new List<double>() { 77500.0, 77800.0 };
+
+            //act
+            var actual = pattern.Check(extremums);
+
+            //assert
+            Assert.IsTrue(actual);            
+        }
+
+        public void CheckTest_1l2()
+        {
+            //arrange
+            var pattern = new PatternPivotPoints_1g2();
+            var extremums = new List<double>() { 77500.0, 77250.0 };
+
+            //act
+            var actual = pattern.Check(extremums);
+
+            //assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void CheckTest_1l2g3()
+        {
+            //arrange
+            var pattern = new PatternPivotPoints_1g2();
+            var extremums = new List<double>() { 77500.0, 77250.0, 77600.0 };
+
+            //act
+            var actual = pattern.Check(extremums);
+
+            //assert
+            Assert.IsTrue(actual);
+        }
+    }
+}
--- TrendByPivotPoints/PatternPivotPoints_1g2l3Tests.cs
+++ TrendByPivotPoints/PatternPivotPoints_1g2l3Tests.cs
@@ -9,35 +9,8 @@ using System.Threading.Tasks;
 namespace TrendByPivotPoints.Tests
 {
     [TestClass()]
-    public class PatternPivotPoints_1l2g3Tests
+    public class PatternPivotPoints_1g2l3Tests
     {
-        [TestMethod()]
-        public void CheckTest_1l2g3()
-        {
-            //arrange
-            var pattern = new PatternPivotPoints_1l2g3();
-            var lows = new List<double>() { 74000.0, 73500.0, 73750.0 };            
-
-            //act
-            var actual = pattern.Check(lows);
-            
-            //assert
-            Assert.IsTrue(actual);
-        }
-
-        [TestMethod()]
-        public void CheckTest_1l2g3g4()
-        {
-            //arrange
-            var pattern = new PatternPivotPoints_1l2g3();
-            var lows = new List<double>() { 74000.0, 73500.0, 73750.0, 74250.0 };
-
-            //act
-            var actual = pattern.Check(lows);
-
-            //assert
-            Assert.IsFalse(actual);
-        }
 
         [TestMethod()]
         public void CheckTest_1g2l3()
@@ -65,6 +38,6 @@ namespace TrendByPivotPoints.Tests
 
             //assert
             Assert.IsFalse(actual);
-        }
+        }       
     }
 }
