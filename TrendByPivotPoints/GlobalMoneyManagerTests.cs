54d102c4e2590a6bef7e166ca9dc161a7599886e
Ilyas Sabitov <wildlucas@yandex.ru>
2021-07-05 00:56:58 +1000

    Улучшаю мани-менеджмент

--- TrendByPivotPoints/GlobalMoneyManagerTests.cs
+++ TrendByPivotPoints/GlobalMoneyManagerTests.cs
@@ -47,5 +47,64 @@ namespace TrendByPivotPoints.Tests
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void GetMoneyTest_deposit1000_riskValuePrcnt5_freeBalance25_returned25_1()
+        {
+            //arrange      
+
+            var startDeposit = 1000d;
+            var stopDeposit = 1100d;
+
+            var profit = stopDeposit - startDeposit;
+
+            if (profit >= 0)
+                startDeposit = startDeposit + 0.5 * profit;
+            else
+                startDeposit = stopDeposit;
+
+
+            var expected = 25;
+            account.FreeBalance = 25;
+
+            //act
+            var actual = globalMoneyManager.GetMoneyForDeal();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void UpdateDeposit(DateTime currentDate, double currentBalance)
+        {
+            var odlDate = deposit.dateTime;
+            if (currentDate.Month != odlDate.Month)
+            {
+                var profit = currentBalance - deposit.balance;
+
+                if (profit >= 0)
+                    deposit.balance = deposit.balance + 0.5 * profit;
+                else
+                    deposit.balance = currentBalance;             
+
+                deposit.dateTime = currentDate;
+            }
+        }
+
+        public void InitializeDeposit()
+        {
+            deposit = new Deposit();
+        }
+
+        Deposit deposit ;        
+    }
+
+    public class Deposit
+    {
+        public DateTime dateTime;
+        public double balance;
+
+        public void Update()
+        {
+
+        }
     }
 }
