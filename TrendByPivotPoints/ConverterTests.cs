9c774097d27338ce854c53fa8ecaa541db9dd9e1
Ilyas Sabitov <wildlucas@yandex.ru>
2022-02-01 14:09:13 +1000

    Реализовал класс Converter и написал к нему тесты. Один тест не проходит. Надо исправить.

---
+++ TrendByPivotPoints/ConverterTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrendByPivotPointsStrategy.Tests
+{
+    [TestClass()]
+    public class ConverterTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void IsGreaterTest1()
+        {
+            //arrange            
+            var expected = true;
+
+            //act
+            var convertable = new Converter(false);            
+            var actual = convertable.IsGreater(4.5, 2.0); ;
+
+            //assert            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsGreaterTest2()
+        {
+            //arrange            
+            var expected = false;
+
+            //act
+            var convertable = new Converter(true);
+            var actual = convertable.IsGreater(4.5, 2.0); ;
+
+            //assert            
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void IsLessTest1()
+        {
+            //arrange            
+            var expected = false;
+
+            //act
+            var convertable = new Converter(false);
+            var actual = convertable.IsLess(4.5, 2.0); ;
+
+            //assert            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsLessTest2()
+        {
+            //arrange            
+            var expected = true;
+
+            //act
+            var convertable = new Converter(true);
+            var actual = convertable.IsLess(4.5, 2.0); ;
+
+            //assert            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TrendByPivotPointsStrategy/Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendByPivotPointsStrategy
+{
+    public class Converter
+    {
+        bool isConverted = false;
+
+        public Converter(bool isConverted)
+        {
+            this.isConverted = isConverted;
+        }
+
+        public bool IsGreater(double a, double b)
+        {
+            if (!isConverted)
+                return a > b;
+            else
+                return b < a;
+        }
+
+        public bool IsLess(double a, double b)
+        {
+            isConverted = !isConverted;
+            var result = IsGreater(a, b);
+            isConverted = !isConverted;
+            return result;
+        }
+    }
+}
