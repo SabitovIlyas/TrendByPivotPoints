43b23f8568b952c2742619c4fae234b64fa546d9
Сабитов Ильяс Ильдарович <wildlucas@yandex.ru>
2024-02-15 14:19:15 +1000

    Научился создавать XML-файл. Теперь буду пробовать его читать.

--- TrendByPivotPoints/XMLExchangeDataTest.cs
+++ TrendByPivotPoints/XMLExchangeDataTest.cs
@@ -10,9 +10,16 @@ namespace TradingSystems.Tests
     {
         [DataTestMethod]
         public void FooTest()
-        {
-            var l = new XElement("Long");
-            var s = new XElement("Short");
+        {            
+            var tradingSystem = new XElement("TradingSystem", new XAttribute("Description", "TradingSystemDonchian5520"),
+                new XElement("Security", "BRH4"),
+                new XElement("PositionSide", "Long"),
+                new XElement("Units", "4")
+                );
+
+            var doc = new XDocument(new XDeclaration("1.0", "utf-16", "yes"), tradingSystem);
+            doc.Save("test.xml");            
+
             Assert.AreEqual(true, true);
         }
     }
