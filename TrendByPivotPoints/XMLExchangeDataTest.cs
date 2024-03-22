091fc44315252ca2a2d0b79a5b466fb5b7d27d92
Сабитов Ильяс Ильдарович <wildlucas@yandex.ru>
2024-03-22 14:03:38 +1000

    Тренируюсь с XML юнит-тесте.

--- TrendByPivotPoints/XMLExchangeDataTest.cs
+++ TrendByPivotPoints/XMLExchangeDataTest.cs
@@ -9,35 +9,40 @@ namespace TradingSystems.Tests
     [TestClass()]
     public class XMLExchangeDataTest
     {
-        [DataTestMethod]
-        public void FooTest()
-        {
-            //var tradingSystems = new List<XElement>()
-            //{
-            //    new XElement("TradingSystem",
-            //    new XAttribute("Description", "TradingSystemDonchianBr"),
-            //    new XAttribute("Security", "BRH4"),
-            //    new XAttribute("PositionSide", "Long"),
-            //    new XElement("Units", "4")
-            //    ),
+        //[DataTestMethod]
+        //public void XMLCreateTest()
+        //{
+        //    var tradingSystems = new XElement
+        //    (
+        //        new XElement
+        //        (
+        //            "TradingSystems",
+        //                new XElement("TradingSystem",
+        //                    new XAttribute("Description", "TradingSystemDonchianBr"),
+        //                    new XAttribute("Security", "BRH4"),
+        //                    new XAttribute("PositionSide", "Long"),
+        //                    new XElement("Units", "4")
+        //                ),
+        //                new XElement("TradingSystem",
+        //                    new XAttribute("Description", "TradingSystemDonchianEu"),
+        //                    new XAttribute("Security", "EuH4"),
+        //                    new XAttribute("PositionSide", "Short"),
+        //                    new XElement("Units", "1")
+        //                )
+        //        )
+        //    );
 
-            //    new XElement("TradingSystem",
-            //    new XAttribute("Description", "TradingSystemDonchianEu"),
-            //    new XAttribute("Security", "EuH4"),
-            //    new XAttribute("PositionSide", "Long"),
-            //    new XElement("Units", "1")
-            //    )
-            //};
+        //    var docOut = new XDocument(new XDeclaration("1.0", "utf-16", "yes"), tradingSystems);
+        //    var fileName = "test.xml";
+        //    docOut.Save(fileName);
 
-            //var docOut = new XDocument(new XDeclaration("1.0", "utf-16", "yes"), tradingSystems);
-            //var fileName = "test.xml";
-            //docOut.Save(fileName);
+        //    var docIn = XDocument.Load(fileName);
 
-            //var docIn = XDocument.Load(fileName);
+        //    var els = docIn.Elements();
 
-            //var els = docIn.Elements();
 
-            Assert.AreEqual(true, true);
-        }
+
+        //    Assert.AreEqual(true, true);
+        //}
     }
 }
