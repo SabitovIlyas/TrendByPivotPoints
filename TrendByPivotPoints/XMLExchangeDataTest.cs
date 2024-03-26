37f01bbe21eefe4655398e432cb0434280c5cff9
Сабитов Ильяс Ильдарович <wildlucas@yandex.ru>
2024-03-26 14:01:33 +1000

    Тренируюсь с XML в юнит-тесте.

--- TrendByPivotPoints/XMLExchangeDataTest.cs
+++ TrendByPivotPoints/XMLExchangeDataTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 
@@ -28,6 +29,12 @@ namespace TradingSystems.Tests
         //                    new XAttribute("Security", "EuH4"),
         //                    new XAttribute("PositionSide", "Short"),
         //                    new XElement("Units", "1")
+        //                ),
+        //                new XElement("TradingSystem",
+        //                    new XAttribute("Description", "TradingSystemDonchianSV"),
+        //                    new XAttribute("Security", "SVH4"),
+        //                    new XAttribute("PositionSide", "Long"),
+        //                    new XElement("Units", "3")
         //                )
         //        )
         //    );
@@ -38,11 +45,31 @@ namespace TradingSystems.Tests
 
         //    var docIn = XDocument.Load(fileName);
 
-        //    var els = docIn.Elements();
+        //    var els = docIn.Elements();            
+            
+        //    foreach (var el in els) 
+        //    {
+        //        Foo(el, els);
 
+        //        foreach(var l in el.Elements())
+        //        {
+        //            Foo(l, el.Elements());
+        //        }
+        //    }            
 
 
         //    Assert.AreEqual(true, true);
         //}
+
+        private void Foo(XElement el, IEnumerable<XElement> els)
+        {
+            var cnt = els.Count();
+            var ths = el;
+            var parent = el.Parent;
+            var fn = el.FirstNode;
+            var ln = el.LastNode;
+            var nn = el.NextNode;
+            var pn = el.PreviousNode;
+        }
     }
 }
