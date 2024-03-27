a1f8a81017d1e6bc80063e9217658b50f349be61
Сабитов Ильяс Ильдарович <wildlucas@yandex.ru>
2024-03-27 13:45:44 +1000

    Пробую работать с XML

--- TrendByPivotPoints/XMLExchangeDataTest.cs
+++ TrendByPivotPoints/XMLExchangeDataTest.cs
@@ -10,56 +10,68 @@ namespace TradingSystems.Tests
     [TestClass()]
     public class XMLExchangeDataTest
     {
-        //[DataTestMethod]
-        //public void XMLCreateTest()
-        //{
-        //    var tradingSystems = new XElement
-        //    (
-        //        new XElement
-        //        (
-        //            "TradingSystems",
-        //                new XElement("TradingSystem",
-        //                    new XAttribute("Description", "TradingSystemDonchianBr"),
-        //                    new XAttribute("Security", "BRH4"),
-        //                    new XAttribute("PositionSide", "Long"),
-        //                    new XElement("Units", "4")
-        //                ),
-        //                new XElement("TradingSystem",
-        //                    new XAttribute("Description", "TradingSystemDonchianEu"),
-        //                    new XAttribute("Security", "EuH4"),
-        //                    new XAttribute("PositionSide", "Short"),
-        //                    new XElement("Units", "1")
-        //                ),
-        //                new XElement("TradingSystem",
-        //                    new XAttribute("Description", "TradingSystemDonchianSV"),
-        //                    new XAttribute("Security", "SVH4"),
-        //                    new XAttribute("PositionSide", "Long"),
-        //                    new XElement("Units", "3")
-        //                )
-        //        )
-        //    );
+        [DataTestMethod]
+        public void XMLCreateTest()
+        {
+            var tradingSystems = new XElement
+            (
+                new XElement
+                (
+                    "TradingSystems",
+                        new XElement("TradingSystem",
+                            new XAttribute("Description", "TradingSystemDonchianBr"),
+                            new XAttribute("Security", "BRH4"),
+                            new XAttribute("PositionSide", "Long"),
+                            new XElement("Units", "4")
+                        ),
+                        new XElement("TradingSystem",
+                            new XAttribute("Description", "TradingSystemDonchianEu"),
+                            new XAttribute("Security", "EuH4"),
+                            new XAttribute("PositionSide", "Short"),
+                            new XElement("Units", "1")
+                        ),
+                        new XElement("TradingSystem",
+                            new XAttribute("Description", "TradingSystemDonchianSV"),
+                            new XAttribute("Security", "SVH4"),
+                            new XAttribute("PositionSide", "Long"),
+                            new XElement("Units", "3")
+                        )
+                )
+            );
 
-        //    var docOut = new XDocument(new XDeclaration("1.0", "utf-16", "yes"), tradingSystems);
-        //    var fileName = "test.xml";
-        //    docOut.Save(fileName);
+            var docOut = new XDocument(new XDeclaration("1.0", "utf-16", "yes"), tradingSystems);
+            var fileName = "test.xml";
+            docOut.Save(fileName);
 
-        //    var docIn = XDocument.Load(fileName);
+            ReadXML();
 
-        //    var els = docIn.Elements();            
+            Assert.AreEqual(true, true);
+        }
+
+        private void ReadXML()
+        {
+            var fileName = "test.xml";
+            var docIn = XDocument.Load(fileName);
+            var root = docIn.Elements();
+            var tradingSystems = root.Elements();
+
+            var longPositions = from ts in tradingSystems
+                                where (string)ts.Attribute("PositionSide") == "Long"
+                                select ts;
+
+            var shortPositions = from ts in tradingSystems
+                                where (string)ts.Attribute("PositionSide") == "Short"
+                                select ts;
+
+            var longValues = from ts in longPositions
+                             select ts.Element("Units");
+
+            var shortValues = from ts in shortPositions
+                             select ts.Element("Units");
             
-        //    foreach (var el in els) 
-        //    {
-        //        Foo(el, els);
-
-        //        foreach(var l in el.Elements())
-        //        {
-        //            Foo(l, el.Elements());
-        //        }
-        //    }            
-
-
-        //    Assert.AreEqual(true, true);
-        //}
+            var totalLongPosition = longValues.Sum(l => int.Parse(l.Value));
+            var totalShortPosition = shortValues.Sum(l => int.Parse(l.Value));
+        }
 
         private void Foo(XElement el, IEnumerable<XElement> els)
         {
