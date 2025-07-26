6827e96f750e1103065ceff3f33732ff7a4234e2
Ilyas Sabitov <wildlucas@yandex.ru>
2025-07-27 01:12:35 +1000

    Продолжаю делать связку форвардного тестирование и генетического алогоритма

--- TrendByPivotPointsOptimizatorTests/ForwardAnalysisAndGeneticAlgorithmTests.cs
+++ TrendByPivotPointsOptimizatorTests/ForwardAnalysisAndGeneticAlgorithmTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
 using System;
+using System.Collections.Generic;
 using System.Runtime;
 using TradingSystems;
 using TrendByPivotPointsOptimizator;
@@ -16,13 +17,25 @@ namespace TrendByPivotPointsOptimizatorTests
             var context = new ContextLab();
             var randomProvider = new RandomProvider();
             var logger = new ConsoleLogger();
-
+            
+            var tickers = new List<Ticker>();
+            var bars = new List<Bar>();
+            var ticker = new Ticker(name: "Brent", Currency.USD, shares: 10, bars, logger,
+                commissionRate: 0);
+            tickers.Add(ticker);
+            var settings = new Settings();
 
             var optimizator = Optimizator.Create();
-            //var ga = new GeneticAlgorithmDonchianChannel(populationSize: 50, generations: 100, 
-            //    crossoverRate: 0.8, mutationRate: 0.1, randomProvider, tickers, settings, context,
-            //    optimizator, logger);
+            var ga = new GeneticAlgorithmDonchianChannel(populationSize: 50, generations: 100, 
+                crossoverRate: 0.8, mutationRate: 0.1, randomProvider, tickers, settings, context,
+                optimizator, logger);
 
+            ga.Initialize(); //создали популяцию хромосом
+
+            var fa = new ForwardAnalysis(genAlg: ga, forwardPeriodDays: 30,
+                backwardPeriodDays: 120, forwardPeriodsCount: 10);
+            
+            
 
         }
     }
