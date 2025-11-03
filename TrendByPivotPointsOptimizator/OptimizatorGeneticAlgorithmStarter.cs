72618ecb742bcb498b1669d5fac04c91a3ef666d
Сабитов Ильяс Ильдарович <sabitov.ilyas@gmail.com>
2025-11-04 01:50:18 +1000

    Поменял параметры оптимизации.

--- TrendByPivotPointsOptimizator/OptimizatorGeneticAlgorithmStarter.cs
+++ TrendByPivotPointsOptimizator/OptimizatorGeneticAlgorithmStarter.cs
@@ -62,7 +62,7 @@ namespace TrendByPivotPointsOptimizator
                 var randomProvider = new RandomProvider();
 
                 var optimizator = Optimizator.Create();
-                var ga = new GeneticAlgorithmDonchianChannel(populationSize: 26, generations: 51,
+                var ga = new GeneticAlgorithmDonchianChannel(populationSize: 27, generations: 51,
                     crossoverRate: 0.8, mutationRate: 0.1, randomProvider, tickers, settings, context,
                     optimizator, loggerNull);//50 //100
 
